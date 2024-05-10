82335b1c3c33dd1cc7b3022b7f2e2f01ca8ad119
Tyler Byrd <tyler.byrd@cvent.com>
2024-05-10 14:56:08 -0400

    Populated the Cards with the Task Data

--- script/todos.js
+++ script/todos.js
@@ -46,7 +46,57 @@ function fetchUserData(event) {
       .then(response => response.json())
       //Get the data
       .then(data => {
-        //Display the Data (its a list of dictionaries)
-        console.log(data);
+        //Loop through the data (For Each Task)
+        data.forEach((task) => {
+            let card = document.createElement('div');
+            card.classList.add('card');
+            card.style.width = '18rem';
+
+            let cardBody = document.createElement('div');
+            cardBody.classList.add('card-body');
+
+            let cardTitle = document.createElement('h5');
+            cardTitle.classList.add('card-title');
+            cardTitle.textContent = 'Task';
+
+            let category = document.createElement('p');
+            category.classList.add('card-text');
+            category.textContent = "Category: " + task.category;
+
+            let description = document.createElement('p');
+            description.classList.add('card-text');
+            description.textContent = "Description: " + task.description;
+
+            let deadline = document.createElement('p');
+            deadline.classList.add('card-text');
+            deadline.textContent = "Deadline: " + task.deadline;
+
+            let priority = document.createElement('p');
+            priority.classList.add('card-text');
+            priority.textContent = "Priority: " + task.priority;
+
+            let completed = document.createElement('p');
+            if (task.completed === true) {
+                completed.innerHTML = "Completed: &#10003";
+            }
+            else if (task.completed === false){
+                completed.innerHTML = "Completed: &#10008";
+            }
+            card.appendChild(completed);
+
+            cardBody.appendChild(cardTitle);
+            cardBody.appendChild(category);
+            cardBody.appendChild(description);
+            cardBody.appendChild(deadline);
+            cardBody.appendChild(priority);
+            cardBody.appendChild(completed);
+
+            card.appendChild(cardBody);
+
+            let container = document.getElementById('container'); 
+            container.appendChild(card);
+        });
+
+
       })
 }
